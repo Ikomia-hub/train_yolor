7dedfdad2a2133cdf129550d326471ffc4ec31f2
ambroise <ambroise.berthe@ikomia.com>
2023-03-22 10:43:59 +0100

    Migration for ikomia 0.9.0

--- train_yolor_process.py
+++ train_yolor_process.py
@@ -195,7 +195,7 @@ class ProcessFactory(dataprocess.CTaskFactory):
         self.info.authors = "Chien-Yao Wang, I-Hau Yeh, Hong-Yuan Mark Liao"
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Detection"
-        self.info.version = "1.0.1"
+        self.info.version = "1.1.0"
         self.info.icon_path = "icons/icon.png"
         self.info.article = "You Only Learn One Representation: Unified Network for Multiple Tasks"
         self.info.journal = "Arxiv"
