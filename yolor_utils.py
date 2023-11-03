8696059713d350deba26662b7245c7d51f47ba83
allankouidri <allan.kouidri@gmail.com>
2023-11-03 12:17:34 +0100

    Fix casting error in divide op during eval

--- yolor_utils.py
+++ yolor_utils.py
@@ -298,6 +298,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         nL = len(labels)  # number of labels
         if nL:
+            labels = labels.astype(np.float32)
             labels[:, 1:5] = xyxy2xywh(labels[:, 1:5])  # convert xyxy to xywh
             labels[:, [2, 4]] /= img.shape[0] # normalized height 0-1
             labels[:, [1, 3]] /= img.shape[1]  # normalized width 0-1
